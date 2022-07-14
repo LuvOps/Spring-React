c718c977ed78d3157a32ec6c305d1fb4f42147a3
Lucas Sousa <lucassusa93@gmail.com>
2022-07-14 18:02:33 -0300

    DatePicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,32 +1,44 @@
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import NotificationButton from '../notificationButton'
-import'./styles.css'
-function SalesCard(){
+import './styles.css'
+function SalesCard() {
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy" />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy" />
                 </div>
             </div>
 
             <div>
                 <table className="dsmeta-sales-table">
                     <thead>
-                        
-                            <tr>
-                                <th className="show992">ID</th>
-                                <th className="show576">Data</th>
-                                <th>Vendedor</th>
-                                <th className="show992">Visitas</th>
-                                <th className="show992">Vendas</th>
-                                <th>Total</th>
-                                <th>Notificar</th>
-                            </tr>
-                        
+
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+
                     </thead>
                     <tbody>
                         <tr>
@@ -38,7 +50,7 @@ function SalesCard(){
                             <td>185523.00</td>
                             <td >
                                 <div className="dsmeta-red-btn-conteiner">
-                                   <NotificationButton/>
+                                    <NotificationButton />
                                 </div>
                             </td>
                         </tr>
@@ -51,7 +63,7 @@ function SalesCard(){
                             <td>185523.00</td>
                             <td>
                                 <div className="dsmeta-red-btn-conteiner">
-                                    <NotificationButton/>
+                                    <NotificationButton />
                                 </div>
                             </td>
                         </tr>
@@ -64,7 +76,7 @@ function SalesCard(){
                             <td>185523.00</td>
                             <td>
                                 <div className="dsmeta-red-btn-conteiner">
-                                    <NotificationButton/>
+                                    <NotificationButton />
                                 </div>
                             </td>
                         </tr>
